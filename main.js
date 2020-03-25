d1ada862c4badb1b96dcb8e9734d0ac712275f9a
Adam Garwacki <adam.garwacki@gmail.com>
2020-03-25 18:19:07 +0100

    Dodano styl w pliku 'main.css', zamieniono elementy '<p>' na '<div>' aby wykluczyć domyślne formatowanie.

--- main.js
+++ main.js
@@ -1,15 +1,11 @@
 document.getElementById('submit').addEventListener("click", displayData);
 
 function displayData() {
-    document.getElementById('name').innerHTML = document.getElementById('enter-name').value;
-    document.getElementById('surname').innerHTML = document.getElementById('enter-surname').value;
-    document.getElementById('phone').innerHTML = document.getElementById('enter-phone').value;
+    document.getElementById('name').innerHTML += document.getElementById('enter-name').value;
+    document.getElementById('surname').innerHTML += document.getElementById('enter-surname').value;
+    document.getElementById('phone').innerHTML += document.getElementById('enter-phone').value;
+
+    document.getElementById('enter-name').value = '';
+    document.getElementById('enter-surname').value = '';
+    document.getElementById('enter-phone').value = '';
 }
-
-
-// displayData(name, surname, phone);
-// let name = document.getElementById('enter-name');
-    // let surname = document.getElementById('enter-surname');
-    // let phone = document.getElementById('enter-phone');
-
-    // console.log(name.value);
